38c856330fb2850a2c0dbc5c53446dbb6c4c4c7c
livingmetal <58518658+livingmetal@users.noreply.github.com>
2026-08-12 17:50:33 +0900

    HG-043: define private model A asset contract

---
+++ apps/web-client/src/character/model-a-private-asset.ts
@@ -0,0 +1,5 @@
+export const MODEL_A_PRIVATE_ASSET = Object.freeze({
+  catalogAssetUrl: "/models/AvatarSample_Y.vrm",
+  fileName: "AvatarSample_Y.vrm",
+  environmentVariable: "HEARTHGHOST_MODEL_A_PATH",
+});
